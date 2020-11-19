8ae2cb94a0f4503d0d03b60334ac1f83e223cd4d
Ruthvikghagarwale <anonymousjoker72219@gmail.com>
2020-11-19 23:26:12 +0530

    call me

---
+++ app/src/main/java/com/example/hiriyurshopy/CategoryAdapter.java
@@ -0,0 +1,62 @@
+package com.example.hiriyurshopy;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class CategoryAdapter {
+
+    // local variable
+    private Context mContext;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super();
+        mContext = context;
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+   
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new Groceries();
+        } else if (position == 1) {
+            return new Fruits();
+        } else if (position == 2) {
+            return new Vegetables();
+        } else {
+            return new ReadyFood();
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+
+    public int getCount() {
+
+        return 4;
+
+    }
+
+
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.Category_grocery);
+        } else if (position == 1) {
+            return mContext.getString(R.string.Category_fruits);
+        } else if (position == 2) {
+            return mContext.getString(R.string.Category_vegetable);
+        } else {
+            return mContext.getString(R.string.Category_ReadyFood);
+        }
+    }
+};
