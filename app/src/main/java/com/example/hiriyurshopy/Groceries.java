6360a0ec63f9f640f7be31675df27252214aa47d
ChinmayeeM-13 <chinmayeem91@gmail.com>
2020-11-22 16:04:43 +0530

    logo code is removed to avoid further errors and probs, so that could be done atlast

--- app/src/main/java/com/example/hiriyurshopy/Groceries.java
+++ app/src/main/java/com/example/hiriyurshopy/Groceries.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
+
 /**
  * A fragment representing a list of Items.
  */
 public class Groceries extends Fragment {
+
 
     public Groceries() {
         // Required empty public constructor
@@ -20,5 +21,8 @@ public class Groceries extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.product_list, container, false);
+
     }
+
+
 }
--- app/src/main/java/com/example/hiriyurshopy/Logo.java
+++ app/src/main/java/com/example/hiriyurshopy/Logo.java
@@ -1,6 +1,5 @@
 package com.example.hiriyurshopy;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,20 +10,5 @@ public class Logo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
 
-        //To set the timer for logo
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(3000);
-                    Intent i = new Intent(getApplicationContext(), Groceries.class);
-                    startActivity(i);
-                }
-                catch(InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
     }
 }
